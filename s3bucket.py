b7d5b7fe5cf9ae171a2b5b9fe61e49950c5f7288
prakashst205 <56536396+prakashst205@users.noreply.github.com>
2019-12-13 13:52:27 +0530

    Update s3bucket.py

--- s3bucket.py
+++ s3bucket.py
@@ -1,3 +1,3 @@
 import boto3
-s3 = boto3.client('s3',region_name='us-east-1')
-s3.create_bucket(Bucket='my-bucket-prakash001')
+s3 = boto3.client('s3',region_name = 'us-east-1')
+s3.create_bucket(Bucket = 'my-bucket-ppppp1')
